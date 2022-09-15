c20b1f2cb5a7ba91bcf5e8935edfed2fc0749cb6
Surya T N <suryatej.nim@yahoo.com>
2022-09-15 10:55:02 +0000

    Implemented the NavBar component to render intended UI layout with the score details, based on whether or not the emoji game is currently in progress

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,1 +1,23 @@
-// Write your code here.
+import './index.css'
+
+const NavBar = props => {
+  const {score, topScore, isGameOn} = props
+
+  return (
+    <nav className="navbar-container">
+      <img
+        className="emoji-game-brand-img"
+        src="https://assets.ccbp.in/frontend/react-js/game-logo-img.png"
+        alt="emoji logo"
+      />
+      {isGameOn && (
+        <div className="score-container">
+          <p className="score-text">Score: {score}</p>
+          <p className="score-text">Top Score: {topScore}</p>
+        </div>
+      )}
+    </nav>
+  )
+}
+
+export default NavBar
