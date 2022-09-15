603c328dc840149d17127b6f5ee6988dcd99f424
Surya T N <suryatej.nim@yahoo.com>
2022-09-15 15:16:38 +0000

    Implemented EmojiCard component to render the intended emoji card UI

--- src/components/EmojiCard/index.js
+++ src/components/EmojiCard/index.js
@@ -1,1 +1,16 @@
-// Write your code here.
+import './index.css'
+
+const EmojiCard = props => {
+  const {cardData, cardSelectionHandler} = props
+  const {id, emojiName, emojiUrl} = cardData
+
+  const onCardSelect = () => cardSelectionHandler(id)
+
+  return (
+    <li className="emoji-card-container" onClick={onCardSelect}>
+      <img className="emoji-card-img" src={emojiUrl} alt={emojiName} />
+    </li>
+  )
+}
+
+export default EmojiCard
